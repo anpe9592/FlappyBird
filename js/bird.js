9156e326f3b854abf43bdf971f63d63eaf44708f
anpe9592 <anpe9592@users.noreply.github.com>
2017-09-28 18:17:51 +0200

    the bird is now falling

--- js/bird.js
+++ js/bird.js
@@ -1,17 +1,26 @@
 class Bird {
     constructor() {
+        this.birdWidth = 34;
+        this.birdHeight = 24;
+
         // create a new Sprite from an image patha
         this.bird = PIXI.Sprite.fromImage('assets/bird/bird-01.png')
 
         // center the sprite's anchor point
         this.bird.anchor.set(0.5);
 
+        this.bird.width = this.birdWidth;
+        this.bird.height = this.birdHeight;
+
         // move the sprite to the center of the screen
         this.bird.x = app.renderer.width / 2;
         this.bird.y = app.renderer.height / 2;
+
+        this.body = Bodies.rectangle(this.bird.x, this.bird.y, this.birdWidth, this.birdHeight);
     }
 
     addBird() {
         app.stage.addChild(this.bird);
+        World.add(engine.world, this.body);
     }
 }
