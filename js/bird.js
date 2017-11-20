d5b3f9d2b91f0b6ce65a0535528756b83ae64c9b
anpe9592 <anpe9592@users.noreply.github.com>
2017-11-20 14:26:23 +0100

    bird moves its wing

--- js/bird.js
+++ js/bird.js
@@ -4,7 +4,14 @@ class Bird {
         this.birdHeight = 24 * 1.2;
 
         // create a new Sprite from an image patha
-        this.bird = PIXI.Sprite.fromImage('assets/bird/bird-01.png')
+        let tmp = [];
+        for (let i = 0; i < 4; i++) { 
+            let num = i + 1; 
+            let tmpBird = PIXI.Texture.fromImage('assets/bird/bird-0'+ num +'.png'); 
+            tmp.push(tmpBird); 
+        } 
+
+        this.bird = new PIXI.extras.AnimatedSprite(tmp); 
 
         // center the sprite's anchor point
         this.bird.anchor.set(0.5);
@@ -15,6 +22,9 @@ class Bird {
         // move the sprite to the center of the screen
         this.bird.x = app.renderer.width / 2;
         this.bird.y = app.renderer.height / 2;
+
+        this.bird.animationSpeed = 0.1;
+        this.bird.loop = false;
 
         this.body = Bodies.rectangle(this.bird.x, this.bird.y, this.birdWidth, this.birdHeight);
     }
@@ -30,6 +40,11 @@ class Bird {
     }
 
     bumpBird() {
+        this.bird.play();
+        this.bird.onComplete = () => { 
+            this.bird.gotoAndStop(0);  
+        }; 
+
         let b = this.body;
         let p = this.body.position;
         let f = { x: 0, y: -0.05 };
